8eea14e34cb646251aeeb494afa384743ad4f532
Dewi Sari <dewi.sari@contracted.sampoerna.com>
2024-12-02 03:19:48 +0000

    add JSON binding functionality

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -7,6 +7,7 @@ sap.ui.define(
   
       return BaseController.extend("zbarcodescan.controller.App", {
         onInit: function() {
+          this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
         }
       });
     }
--- webapp/controller/ViewBarcode.controller.js
+++ webapp/controller/ViewBarcode.controller.js
@@ -1,12 +1,64 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
-function (Controller) {
+
+/**
+ * 
+ * @param {typeof sap.ui.core.mvc.Controller} Controller 
+ * @param {typeof sap.ui.model.json.JSONModel} JSONModel 
+ * @returns 
+ */
+function (Controller, JSONModel) {
     "use strict";
+    
+    async function fetchData(uri) {
+       const response = await fetch(uri);
+       const data = await response.json();
+       return data;
+    }
+
+    const oModel = new JSONModel();
+    let prefixId;
 
     return Controller.extend("zbarcodescan.controller.ViewBarcode", {
         onInit: function () {
+            this.getView().setModel(oModel, "barcode");
+            prefixId = this.createId();
+            console.log(prefixId);
+            if (prefixId){
+                prefixId = 
+                    `${prefixId.split("ViewBarcode--")[0]}ViewBarcode--`;
+            } else {
+                prefixId = "";
+            }
+            console.log(prefixId);
 
+        },
+
+        onScanSuccess: function (oEvent) {
+            if (oEvent.getParameter("cancelled")) {
+                MessageToast.show("Scan cancelled", { duration:1000 });
+            } else {
+                if (oEvent.getParameter("text")) {
+                    const barcodeNum = oEvent.getParameter("text");
+                    fetch('/model/data.json')
+                        .then(response => response.json())
+                        .then(data => {
+                            console.log(data);
+                            const item = data.Barcode.find(barcode => barcode.Number === barcodeNum);
+                            oModel.setData(item);
+                        })
+                    // console.log(oData);
+                    // const item = oData.Barcode.find(barcode => barcode.Number === barcodeNum);
+                    // oModel.setData(item);
+
+                }
+            }
+        },
+
+        onScanError: function(oEvent) {
+            MessageToast.show("Scan failed: " + oEvent, { duration:1000 });
         }
     });
 });
